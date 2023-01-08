799ffb7048e473b486b40e108fbe57707abff033
seongmin shin <62044613+seongm2n@users.noreply.github.com>
2023-01-08 21:20:12 +0900

    Add files via upload

--- js/background.js
+++ js/background.js
@@ -1,12 +1,14 @@
 const button = document.querySelector(".changeBtn");
 const images = [
-    "0.jpg","1.jpg","2.jpg"
+    "0.JPG","1.JPG","2.JPG","3.JPG","4.JPG","5.JPG","6.JPG","7.JPG"
 ];
 
 function changeImg() {
     const chosenImg = images[Math.floor(Math.random() * images.length)];
     document.body.style.background = `url(img/bgImg/${chosenImg})`;
     document.body.style.backgroundRepeat = `no-repeat`;
+    document.body.style.backgroundSize = `cover`;
+
 
 
 }
