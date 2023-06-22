cf7f3ae2189c2d1204869b8e114df96e7a1dae5d
seongm2n <ssongpig@naver.com>
2023-06-22 17:23:01 +0900

    fix : login 했을 때 사라지는 form 사라지지 않게 수정

--- js/greeting.js
+++ js/greeting.js
@@ -1,30 +1,30 @@
-const loginForm = document.querySelector("#login-form")
-const loginInput = document.querySelector("#login-form input"); 
-const greeting = document.querySelector("#greeting");
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
 
-const HIDDEN_CLASSNAME = "hidden"; //string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용 
-const USERNAME_KEY = "username";
-
+const HIDDEN_CLASSNAME = 'hidden'; //string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용
+const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
-    event.preventDefault(); //브라우저의 기본 동작을 막고, 그 동작 : 페이지 새로고침
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+	event.preventDefault(); //브라우저의 기본 동작을 막고, 그 동작 : 페이지 새로고침
+	loginForm.remove();
+	// loginForm.classList.add(HIDDEN_CLASSNAME);
+	const username = loginInput.value;
+	localStorage.setItem(USERNAME_KEY, username);
+	paintGreetings(username);
 }
 
-function paintGreetings(username){
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+function paintGreetings(username) {
+	greeting.innerText = `Hello ${username}`;
+	greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-if(savedUsername === null){
-    // show the form
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
-}else {
-    // show the greetings
-    paintGreetings(savedUsername);
+if (savedUsername === null) {
+	// show the form
+	loginForm.classList.remove(HIDDEN_CLASSNAME);
+	loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+	// show the greetings
+	paintGreetings(savedUsername);
 }
